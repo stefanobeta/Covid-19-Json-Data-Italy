af4710e89b5cdc431e46da6215c3b4db50ff6d6b
Stefano <33597946+stefanobeta@users.noreply.github.com>
2020-03-20 14:52:19 +0100

    Update getDataExample.php

--- getDataExample.php
+++ getDataExample.php
@@ -10,7 +10,7 @@ $data = json_decode($json, true);
 /*
 
  $data stores the values: [date][infos];
- - date is a String format ['YYYY-MM-DD']
+ - date is in a String format ['YYYY-MM-DD']
  - infos are Int and can be:
  	['confirmed']: total of cases till the date you choose
  	['positive']: number of positive people till the date you choose
