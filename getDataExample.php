655792acb8fbd91c09d1de5dd7a8035529b5b6de
Stefano <33597946+stefanobeta@users.noreply.github.com>
2020-03-20 14:40:16 +0100

    Add files via upload

---
+++ getDataExample.php
@@ -0,0 +1,24 @@
+<?php
+
+// Json URL
+$url = 'https://github.com/stefanobeta/Covid-19-Json-Data-Italy/raw/master/covid.json';
+
+// Get Json
+$json = file_get_contents($url);
+$data = json_decode($json, true);
+
+/*
+
+ $data stores the values: [date][infos];
+ - date is a Sring format ['YYYY-MM-DD']
+ - infos are Int and can be:
+ 	['confirmed']: total of cases till the date you choose
+ 	['positive']: number of positive people till the date you choose
+ 	['deaths']: number of deaths till the date you choose
+ 	['recovered']: number of recovered people till the date you choose
+
+*/
+
+$recoveredOn18thOfMarch = $data['2020-03-18']['recovered'];
+echo 'Recovered people on 18th of March in Italy are '.$recoveredOn18thOfMarch;
+
